46e67f2de23e60f44fab8d70e1491d55050b8f06
Val Bancer <valerij.bancer@gmail.com>
2026-08-23 10:48:27 +0200

    Clean up SparkDictActivity (#22)

--- app/src/androidTest/java/net/bancer/sparkdict/SparkDictActivityTest.java
+++ app/src/androidTest/java/net/bancer/sparkdict/SparkDictActivityTest.java
@@ -1,8 +1,10 @@
-package net.bancer.sparkdict.test;
+package net.bancer.sparkdict;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
@@ -14,14 +16,20 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static net.bancer.sparkdict.BaseActivity.PREFS_NAME;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.Spanned;
+import android.text.style.BackgroundColorSpan;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.test.core.app.ActivityScenario;
@@ -30,21 +38,19 @@ import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
-import net.bancer.sparkdict.DictManagerActivity;
-import net.bancer.sparkdict.R;
-import net.bancer.sparkdict.SparkDictActivity;
 import net.bancer.sparkdict.domain.core.Shelf;
 import net.bancer.sparkdict.views.SearchInputField;
 
-import org.junit.Before;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-
-import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -54,42 +60,24 @@ import java.util.Set;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SparkDictActivityTest {
 
-    public static final int INIT_ARTICLES_COUNT = 0;
-
-    private static final String SEARCH_STRING = "interface";
-
     @Rule
     public ActivityScenarioRule<SparkDictActivity> mActivity = new ActivityScenarioRule<>(SparkDictActivity.class);
 
-    private SearchInputField mInputTextView;
-
-    private ImageButton mSearchButton;
-
-    private ProgressBar mSearchProgress;
-
-    private LinearLayout mArticlesList;
-
-    private Shelf mShelf;
-
-    @Before
-    public void setUp() {
-        mActivity.getScenario().onActivity(activity -> {
-            this.mInputTextView = activity.findViewById(R.id.searchTextView);
-            this.mSearchButton = activity.findViewById(R.id.searchButton);
-            this.mSearchProgress = activity.findViewById(R.id.search_progress);
-            this.mArticlesList = activity.findViewById(R.id.articles_list);
-            this.mShelf = activity.getShelf();
-        });
-    }
-
     @Test
     public void testPreConditions() {
-        assertNotNull(mInputTextView.getAdapter());
-        assertNotNull(mSearchButton);
-        assertNotNull(mSearchProgress);
-        assertNotNull(mArticlesList);
-        assertEquals(INIT_ARTICLES_COUNT, mArticlesList.getChildCount());
-        assertNotNull(mShelf);
+        mActivity.getScenario().onActivity(activity -> {
+            SearchInputField mInputTextView = activity.findViewById(R.id.searchTextView);
+            ImageButton mSearchButton = activity.findViewById(R.id.searchButton);
+            ProgressBar mSearchProgress = activity.findViewById(R.id.search_progress);
+            LinearLayout mArticlesList = activity.findViewById(R.id.articles_list);
+            Shelf mShelf = activity.getShelf();
+            assertNotNull(mInputTextView.getAdapter());
+            assertNotNull(mSearchButton);
+            assertNotNull(mSearchProgress);
+            assertNotNull(mArticlesList);
+            assertEquals(0, mArticlesList.getChildCount());
+            assertNotNull(mShelf);
+        });
     }
 
     @Test
@@ -100,9 +88,9 @@ public class SparkDictActivityTest {
         onView(withId(R.id.search_progress))
             .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
         onView(withId(R.id.searchTextView))
-            .perform(typeText(SEARCH_STRING));
+            .perform(typeText("interface"));
         onView(withId(R.id.searchTextView))
-            .check(matches(withText(SEARCH_STRING)));
+            .check(matches(withText("interface")));
         onView(withId(R.id.searchTextView))
             .perform(pressImeActionButton());
         onView(withId(R.id.searchTextView))
@@ -110,6 +98,82 @@ public class SparkDictActivityTest {
         // Assert that the progress bar is not visible after the search.
         onView(withId(R.id.search_progress))
             .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+    }
+
+    @Test
+    public void testFindOnPage() throws InterruptedException {
+        // search definitions of "go"
+        onView(withId(R.id.searchTextView))
+            .perform(typeText("go"));
+        onView(withId(R.id.searchTextView))
+            .perform(pressImeActionButton());
+        Thread.sleep(2000);
+        // check that the spinning will is not displayed any more
+        onView(withId(R.id.search_progress))
+            .check(matches(not(isDisplayed())));
+        // click on the 3 dots menu button
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        onView(withText("Find On Page"))
+            .perform(click());
+        // enter "went" into "find on page" input
+        onView(withId(R.id.find_on_page_edit_text))
+            .perform(typeText("went"));
+        // click on ▼ button
+        onView(withId(R.id.find_on_page_next_btn))
+            .perform(click());
+        // check that "went" is highlighted
+        onView(allOf(
+            withId(R.id.definitions_body),
+            hasHighlightedWord("went")
+        ))
+            .check(matches(isDisplayed()));
+        // enter "gone" into "find on page" input
+        onView(withId(R.id.find_on_page_edit_text))
+            .perform(replaceText("gone"));
+        // click on ▲ button
+        onView(withId(R.id.find_on_page_previous_btn))
+            .perform(click());
+        // check that "gone" is highlighted
+        onView(allOf(
+            withId(R.id.definitions_body),
+            hasHighlightedWord("gone")
+        ))
+            .check(matches(isDisplayed()));
+        // click on ✕ button
+        onView(withId(R.id.find_on_page_close_btn))
+            .perform(click());
+    }
+
+    /**
+     * Creates a matcher that checks whether a {@link TextView} contains the specified
+     * word with a {@link BackgroundColorSpan} applied to it.
+     *
+     * @param word The word to check for.
+     * @return A matcher that matches a {@link TextView} containing the highlighted word.
+     */
+    private static Matcher<View> hasHighlightedWord(String word) {
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            protected boolean matchesSafely(View view) {
+                TextView textView = (TextView) view;
+                if (!(textView.getText() instanceof Spanned)) {
+                    return false;
+                }
+                Spanned text = (Spanned) textView.getText();
+                int start = text.toString().indexOf(word);
+                if (start < 0) {
+                    return false;
+                }
+                int end = start + word.length();
+                return text.getSpans(start, end, BackgroundColorSpan.class).length > 0;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("contains highlighted \"" + word + "\"");
+            }
+        };
     }
 
     @Test
