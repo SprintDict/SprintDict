4b86d10cb3492de9645dd20d27787041e7612287
bancer <val@dinnerbooking.com>
2026-08-27 21:54:40 +0200

    Reformat SearchInputField

--- app/src/main/java/net/bancer/sparkdict/views/SearchInputField.java
+++ app/src/main/java/net/bancer/sparkdict/views/SearchInputField.java
@@ -10,42 +10,39 @@ import android.widget.AutoCompleteTextView;
 /**
  * SearchInputField is a form field where the user can type a word to be found
  * in the dictionaries.
- * 
- * @author Valerij Bancer
- *
  */
 @SuppressLint("AppCompatCustomView")
 public class SearchInputField extends AutoCompleteTextView {
 
-	public SearchInputField(Context context) {
-		super(context);
-	}
+    public SearchInputField(Context context) {
+        super(context);
+    }
 
-	public SearchInputField(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public SearchInputField(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public SearchInputField(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
+    public SearchInputField(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
 
-	@Override
-	public boolean onKeyPreIme(int keyCode, KeyEvent event) {
-		if (isPopupShowing()) {
-			if(keyCode == KeyEvent.KEYCODE_BACK) {
-				if(event.getAction() == KeyEvent.ACTION_UP) {
-					InputMethodManager imm = (InputMethodManager) 
-							getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-					//hide keyboard
-					boolean keyboardHided = imm.hideSoftInputFromWindow(getWindowToken(), 0);
-					//if there was no keyboard to hide then hide the drop down list
-					if(!keyboardHided) {
-						dismissDropDown();
-					}
-				}
-				return true;
-			}
-		}
-		return super.onKeyPreIme(keyCode, event);
-	}
+    @Override
+    public boolean onKeyPreIme(int keyCode, KeyEvent event) {
+        if (isPopupShowing()) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (event.getAction() == KeyEvent.ACTION_UP) {
+                    InputMethodManager imm = (InputMethodManager)
+                        getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    //hide keyboard
+                    boolean keyboardHided = imm.hideSoftInputFromWindow(getWindowToken(), 0);
+                    //if there was no keyboard to hide then hide the drop down list
+                    if (!keyboardHided) {
+                        dismissDropDown();
+                    }
+                }
+                return true;
+            }
+        }
+        return super.onKeyPreIme(keyCode, event);
+    }
 }
