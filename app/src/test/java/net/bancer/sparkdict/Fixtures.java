8c6b19ffcc762fe56311bc3044b0a494f3517d43
bancer <val@dinnerbooking.com>
2026-09-03 22:41:14 +0200

    Clean up after refactorings

--- app/src/test/java/net/bancer/sparkdict/Fixtures.java
+++ app/src/test/java/net/bancer/sparkdict/Fixtures.java
@@ -14,30 +14,26 @@ public final class Fixtures {
     public static final String GCIDE_IFO_FILE_RELATIVE = GCIDE_DICT_FOLDER + "/stardict.ifo";
     public static final String GCIDE_IFO_FILE = TEST_DATA_PATH + GCIDE_IFO_FILE_RELATIVE;
     public static final String GCIDE_IDX_FILE = TEST_DATA_PATH + GCIDE_DICT_FOLDER + "/stardict.idx";
-    public static final String GCIDE_DICT_DZ_FILE_RELATIVE = GCIDE_DICT_FOLDER + "/stardict.dict.dz";
-    public static final String GCIDE_DICT_DZ_FILE = TEST_DATA_PATH + GCIDE_DICT_DZ_FILE_RELATIVE;
+    public static final String GCIDE_DICT_DZ_FILE = GCIDE_DICT_FOLDER + "/stardict.dict.dz";
     public static final String ALL_FIELDS_IFO_FILE = TEST_DATA_PATH + "all-fields-ifo/stardict.ifo";
 
     public static final String DUMMY_TM_DICT_FOLDER = "dummy-dict-tm-type";
 
-    public static final String DUMMY_TM_IFO_FILE_RELATIVE = DUMMY_TM_DICT_FOLDER + "/stardict.ifo";
-    public static final String DUMMY_TM_IFO_FILE = TEST_DATA_PATH + DUMMY_TM_IFO_FILE_RELATIVE;
+    public static final String DUMMY_TM_IFO_FILE = DUMMY_TM_DICT_FOLDER + "/stardict.ifo";
 
-    public static final String DUMMY_TM_IDX_FILE = TEST_DATA_PATH + DUMMY_TM_DICT_FOLDER + "/stardict.idx";
+    public static final String DUMMY_TM_IDX_FILE = DUMMY_TM_DICT_FOLDER + "/stardict.idx";
 
-    public static final String DUMMY_TM_DICT_DZ_FILE = TEST_DATA_PATH + DUMMY_TM_DICT_FOLDER + "/stardict.dict.dz";
+    public static final String DUMMY_TM_DICT_DZ_FILE = DUMMY_TM_DICT_FOLDER + "/stardict.dict.dz";
 
     public static final String DUMMY_MULTI_DICT_FOLDER = "dymmy-dict-multi-type";
 
-    public static final String DUMMY_MULTI_IFO_FILE_RELATIVE = DUMMY_MULTI_DICT_FOLDER + "/stardict.ifo";
-    public static final String DUMMY_MULTI_IFO_FILE = TEST_DATA_PATH + DUMMY_MULTI_IFO_FILE_RELATIVE;
+    public static final String DUMMY_MULTI_IFO_FILE = DUMMY_MULTI_DICT_FOLDER + "/stardict.ifo";
 
-    public static final String DUMMY_MULTI_IDX_FILE = TEST_DATA_PATH + DUMMY_MULTI_DICT_FOLDER + "/stardict.idx";
+    public static final String DUMMY_MULTI_IDX_FILE = DUMMY_MULTI_DICT_FOLDER + "/stardict.idx";
 
-    public static final String DUMMY_MULTI_DICT_DZ_FILE = TEST_DATA_PATH + DUMMY_MULTI_DICT_FOLDER + "/stardict.dict.dz";
+    public static final String DUMMY_MULTI_DICT_DZ_FILE = DUMMY_MULTI_DICT_FOLDER + "/stardict.dict.dz";
 
-    public static final String DUMMY_MULTI_RES_ZIP_FILE_RELATIVE = DUMMY_MULTI_DICT_FOLDER + "/res.zip";
-    public static final String DUMMY_MULTI_RES_ZIP_FILE = TEST_DATA_PATH + DUMMY_MULTI_RES_ZIP_FILE_RELATIVE;
+    public static final String DUMMY_MULTI_RES_ZIP_FILE = DUMMY_MULTI_DICT_FOLDER + "/res.zip";
 
     public static void buildSparkDictIndex() throws IOException {
         DictionaryFiles dictionaryFiles = new FileDictionaryFiles(Fixtures.TEST_DATA_PATH);
@@ -55,28 +51,28 @@ public final class Fixtures {
 
     public static void buildDummyTmDictIndex() throws IOException {
         DictionaryFiles dictionaryFiles = new FileDictionaryFiles(Fixtures.TEST_DATA_PATH);
-        BookInfo bookInfo = new BookInfo(Fixtures.DUMMY_TM_IFO_FILE_RELATIVE, dictionaryFiles);
+        BookInfo bookInfo = new BookInfo(Fixtures.DUMMY_TM_IFO_FILE, dictionaryFiles);
         SparkDictIndex index = new SparkDictIndex(bookInfo);
         index.buildIndex();
     }
 
     public static void deleteDummyTmDictIndex() {
         DictionaryFiles dictionaryFiles = new FileDictionaryFiles(Fixtures.TEST_DATA_PATH);
-        BookInfo bookInfo = new BookInfo(Fixtures.DUMMY_TM_IFO_FILE_RELATIVE, dictionaryFiles);
+        BookInfo bookInfo = new BookInfo(Fixtures.DUMMY_TM_IFO_FILE, dictionaryFiles);
         SparkDictIndex index = new SparkDictIndex(bookInfo);
         index.delete();
     }
 
     public static void buildDummyMultiDictIndex() throws IOException {
         DictionaryFiles dictionaryFiles = new FileDictionaryFiles(Fixtures.TEST_DATA_PATH);
-        BookInfo bookInfo = new BookInfo(Fixtures.DUMMY_MULTI_IFO_FILE_RELATIVE, dictionaryFiles);
+        BookInfo bookInfo = new BookInfo(Fixtures.DUMMY_MULTI_IFO_FILE, dictionaryFiles);
         SparkDictIndex index = new SparkDictIndex(bookInfo);
         index.buildIndex();
     }
 
     public static void deleteDummyMultiDictIndex() {
         DictionaryFiles dictionaryFiles = new FileDictionaryFiles(Fixtures.TEST_DATA_PATH);
-        BookInfo bookInfo = new BookInfo(Fixtures.DUMMY_MULTI_IFO_FILE_RELATIVE, dictionaryFiles);
+        BookInfo bookInfo = new BookInfo(Fixtures.DUMMY_MULTI_IFO_FILE, dictionaryFiles);
         SparkDictIndex index = new SparkDictIndex(bookInfo);
         index.delete();
     }
